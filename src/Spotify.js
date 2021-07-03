11dafb4f7b094bb2acc47ec106f9b2e5360b0fac
Mackenzie Huynh <mhuynh2@ucsc.edu>
2021-07-02 19:11:14 -0700

    spotify init

---
+++ src/Spotify.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default class Spotify extends React.Component {
+
+    state = {
+
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+
+}
